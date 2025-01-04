f98d090c56e1e2e428f0d259122575f39ad28dda
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2025-01-04 18:16:48 -0300

    Value Object de slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("it should be able to create new slug from text", () => {
+  const slug = Slug.createFromText("Slug text example");
+
+  expect(slug.value).toEqual("slug-text-example");
+});
