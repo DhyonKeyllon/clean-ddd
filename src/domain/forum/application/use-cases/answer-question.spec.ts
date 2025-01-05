7f70815b70c83f074f70141d4ac5e13ea09bf59a
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2025-01-05 12:27:10 -0300

    refactor: refactor unit tests

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,18 +1,26 @@
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
 import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answers-repository";
 
-const fakeAnswerRepository: AnswersRepository = {
-  create: async () => {},
-};
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: AnswerQuestionUseCase; // system under test
 
-test("should create a answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
-
-  const { answer } = await answerQuestion.execute({
-    instructorId: "1",
-    questionId: "1",
-    content: "Nova resposta",
+describe("Create Question", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository);
   });
 
-  expect(answer.content).toEqual("Nova resposta");
+  test("should be able to create an answer", async () => {
+    const { answer } = await sut.execute({
+      instructorId: "1",
+      questionId: "1",
+      content: "Conteúdo da resposta",
+    });
+
+    expect(answer.id).toBeTruthy();
+
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id);
+
+    expect(answer.content).toEqual("Conteúdo da resposta");
+  });
 });
--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,20 +1,28 @@
-import { QuestionsRepository } from "../repositories/questions-repository";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 import { CreateQuestionUseCase } from "./create-question";
 
-const fakeCreateQuestRepository: QuestionsRepository = {
-  create: async () => {},
-};
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: CreateQuestionUseCase; // system under test
 
-test("should create a question", async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeCreateQuestRepository);
-
-  const { question } = await createQuestion.execute({
-    authorId: "1",
-    title: "Título da Nova pergunta",
-    content: "Conteúdo da nova pergunta",
+describe("Create Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository);
   });
 
-  expect(question.title).toEqual("Título da Nova pergunta");
-  expect(question.content).toEqual("Conteúdo da nova pergunta");
-  expect(question.slug.value).toEqual("titulo-da-nova-pergunta");
+  test("should be able to create an question", async () => {
+    const { question } = await sut.execute({
+      authorId: "1",
+      title: "Título da Nova pergunta",
+      content: "Conteúdo da nova pergunta",
+    });
+
+    expect(question.id).toBeTruthy();
+
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id);
+
+    expect(question.title).toEqual("Título da Nova pergunta");
+    expect(question.content).toEqual("Conteúdo da nova pergunta");
+    expect(question.slug.value).toEqual("titulo-da-nova-pergunta");
+  });
 });
