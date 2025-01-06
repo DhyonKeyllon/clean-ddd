45f99bf810bda50fd9059e28ef58f622df0f1941
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2025-01-06 15:18:56 -0300

    feat: add comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { makeQuestion } from "test/factories/make-question";
+import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUseCase; // system under test
+
+describe("Choose Question Best Answer", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    );
+  });
+
+  test("should be able to comment on question", async () => {
+    const question = makeQuestion();
+
+    await inMemoryQuestionsRepository.create(question);
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: "author-1",
+      content: "Content text example",
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      "Content text example",
+    );
+  });
+});
